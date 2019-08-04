d7b9c43c4533dd21fc82fa348a29c5d9b767f9f1
Felipe <feliperomao.a@gmail.com>
2019-08-03 22:32:19 -0300

    push production

---
+++ build/precache-manifest.82c28697592357030d24093464b94383.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb906fb8cfeb92b358b227b2a1f7ee4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db286557741fd175d522",
+    "url": "/static/js/2.5d4b27f1.chunk.js"
+  },
+  {
+    "revision": "7d818b3f1d4d341b8016",
+    "url": "/static/js/main.3422311c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
